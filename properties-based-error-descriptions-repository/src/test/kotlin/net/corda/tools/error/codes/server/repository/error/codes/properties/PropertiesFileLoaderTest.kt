3e4b4da2863dd0151ebacd601a318f067965ca4a
Michele Sollecito <michele.sollecito@r3.com>
2018-10-01 12:05:31 +0100

    PropertiesFileLoaderTest.kt

---
+++ properties-based-error-descriptions-repository/src/test/kotlin/net/corda/tools/error/codes/server/repository/error/codes/properties/PropertiesFileLoaderTest.kt
@@ -0,0 +1,60 @@
+package net.corda.tools.error.codes.server.repository.error.codes.properties
+
+import net.corda.tools.error.codes.server.domain.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import java.io.File
+import java.net.URI
+import java.nio.file.Files
+import java.util.*
+
+internal class PropertiesFileLoaderTest {
+
+    private val temporaryFile: File = Files.createTempFile("error_code", ".properties").toFile().also(File::deleteOnExit)
+
+    @Test
+    fun empty_file_results_in_no_descriptions() {
+
+        val errorCode = ErrorCode("1uoda")
+        val descriptions: Flux<out ErrorDescription> = Flux.empty()
+
+        val properties = listOf(errorCode to descriptions).toProperties().also { it.writeToFile(temporaryFile) }
+        val loader = loaderFrom(temporaryFile)
+
+        val loadedProperties = loader.load()
+
+        assertThat(loadedProperties).isEqualTo(properties)
+    }
+
+    @Test
+    fun file_with_one_line_results_in_one_description() {
+
+        val errorCode = ErrorCode("1uoda")
+        val description1 = ErrorDescription(ErrorDescriptionLocation.External(URI.create("https//a.com")), ErrorCoordinates(errorCode, ReleaseVersion(3, 2, 1), PlatformEdition.Enterprise))
+        val description2 = ErrorDescription(ErrorDescriptionLocation.External(URI.create("https//b.com")), ErrorCoordinates(errorCode, ReleaseVersion(1, 3, 2), PlatformEdition.OpenSource))
+        val descriptions: Flux<out ErrorDescription> = Flux.just(description1, description2)
+
+        val properties = listOf(errorCode to descriptions).toProperties().also { it.writeToFile(temporaryFile) }
+        val loader = loaderFrom(temporaryFile)
+
+        val loadedProperties = loader.load()
+
+        assertThat(loadedProperties).isEqualTo(properties)
+    }
+
+    private fun loaderFrom(file: File): PropertiesFileLoader {
+
+        return PropertiesFileLoader(object : PropertiesFileLoader.Configuration {
+
+            override val propertiesFile = file
+        })
+    }
+
+    private fun Properties.writeToFile(file: File) {
+
+        file.outputStream().use {
+            store(it, null)
+        }
+    }
+}

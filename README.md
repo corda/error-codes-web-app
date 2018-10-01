238671eef854360c8e356a7f2afd71bf6bb8eb9a
Michele Sollecito <michele.sollecito@r3.com>
2018-10-01 15:16:35 +0100

    settings.gradle switched to use Kotlin Gradle DSL.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Run/Debug `ErrorCodesWebAppStarter.kt` from Intellij.
 ## API
 
 By default, the error location is available at `http://localhost:8080/editions/[OS|ENT]/releases/<release_version>[e.g., 3.2.1]/errors/<error_code>`.
+
+Configuration could be optionally set to a specific YAML file by passing `-Dconfiguration.file.path=/<path_to_file>.yml` as system property, or by setting `configuration.file.path=/<path_to_file>.yml` as an environmental variable.
--- starter/src/main/kotlin/net/corda/tools/error/codes/server/ConfigLocatingDecorator.kt
+++ starter/src/main/kotlin/net/corda/tools/error/codes/server/ConfigLocatingDecorator.kt
@@ -1,6 +1,8 @@
 package net.corda.tools.error.codes.server
 
 import com.uchuhimo.konf.Config
+import com.uchuhimo.konf.ConfigSpec
+import java.nio.file.Paths
 import javax.inject.Named
 
 @Named
@@ -8,8 +10,24 @@ internal class ConfigLocatingDecorator : (Config) -> Config {
 
     private companion object {
 
-        private const val CONFIGURATION_FILE_NAME = "configuration.yml"
+        private const val CONFIGURATION_FILE = "configuration.file"
+        private const val CONFIGURATION_FILE_PATH = "path"
+        private const val CONFIGURATION_FILE_RESOURCE_NAME = "configuration.yml"
+
+        private object Spec : ConfigSpec(CONFIGURATION_FILE) {
+
+            val path by optional<String?>(name = CONFIGURATION_FILE_PATH, default = null)
+        }
+
+        private val rootConfig = Config { addSpec(Spec) }.from().env().from().systemProperties()
     }
 
-    override fun invoke(config: Config): Config = config.from.yaml.resource(CONFIGURATION_FILE_NAME).from.env().from.systemProperties()
+    override fun invoke(config: Config): Config = (configPath() ?: ::fromResource).invoke(config).from.env().from.systemProperties()
+
+    private fun configPath(): ((Config) -> Config)? {
+
+        return rootConfig[Spec.path]?.let { { config: Config -> config.from.yaml.file(Paths.get(it).toAbsolutePath().toFile()) } }
+    }
+
+    private fun fromResource(config: Config): Config = config.from.yaml.resource(CONFIGURATION_FILE_RESOURCE_NAME)
 }

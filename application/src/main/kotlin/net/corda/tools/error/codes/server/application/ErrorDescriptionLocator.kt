d18198d8cb1e8fd38b34aeb6cb6df387981c383c
Anthony Keenan <anthony.keenan@r3.com>
2018-11-15 16:16:00 +0000

    Delete unused file

--- application/src/main/kotlin/net/corda/tools/error/codes/server/application/ErrorDescriptionLocator.kt
+++
@@ -1,7 +0,0 @@
-package net.corda.tools.error.codes.server.application
-
-import net.corda.tools.error.codes.server.application.annotations.Application
-import net.corda.tools.error.codes.server.domain.*
-import reactor.core.publisher.Mono
-import javax.inject.Inject
-import javax.inject.Named
